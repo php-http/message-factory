88b582321ce05a287a1616b403c97e7a318fd19a
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-05-19 17:42:55 +0200

    Adds ClientFactory

---
+++ src/ClientFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Http Message Factory package.
+ *
+ * (c) PHP HTTP Team
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Http\Message;
+
+use Psr\Http\Message\UriInterface;
+
+/**
+ * All the factory components that could be used in client context (like an HTTP Client)
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+interface ClientFactory extends MessageFactory, StreamFactory, UriFactory
+{
+
+}
