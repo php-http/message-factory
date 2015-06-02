495ad55198671f570aa1db9dcc94ab6fa3584d61
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-06-02 20:39:40 +0200

    Updates LICENSE

--- src/UriFactoryAwareTemplate.php
+++ src/UriFactoryAwareTemplate.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Http Message Factory package.
  *
- * (c) PHP HTTP Team
+ * (c) PHP HTTP Team <team@php-http.org>
  *
  * For the full copyright and license information, please read the LICENSE
  * file that was distributed with this source code.
