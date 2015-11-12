9141e266d740ef6dc44b30f4800fea966596ca65
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-11-12 09:18:13 +0100

    Remove file header

--- src/MessageFactory.php
+++ src/MessageFactory.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Http Message Factory package.
- *
- * (c) PHP HTTP Team <team@php-http.org>
- *
- * For the full copyright and license information, please read the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Http\Message;
 
 use Psr\Http\Message\UriInterface;
