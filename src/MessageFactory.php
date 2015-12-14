cd888768b94c8df4d6c86f36e1754a2cf7ac3476
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-12-14 13:30:29 +0100

    Extract Response factory from Message factory
    Applied fixes from StyleCI
    
    Fix wrong extension
    
    Applied fixes from StyleCI

--- src/MessageFactory.php
+++ src/MessageFactory.php
@@ -4,7 +4,6 @@ namespace Http\Message;
 
 use Psr\Http\Message\UriInterface;
 use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 
 /**
@@ -12,7 +11,7 @@ use Psr\Http\Message\StreamInterface;
  *
  * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
  */
-interface MessageFactory
+interface MessageFactory extends ResponseFactory
 {
     /**
      * Creates a new PSR-7 request.
@@ -28,25 +27,6 @@ interface MessageFactory
     public function createRequest(
         $method,
         $uri,
-        array $headers = [],
-        $body = null,
-        $protocolVersion = '1.1'
-    );
-
-    /**
-     * Creates a new PSR-7 response.
-     *
-     * @param int                                  $statusCode
-     * @param string|null                          $reasonPhrase
-     * @param array                                $headers
-     * @param resource|string|StreamInterface|null $body
-     * @param string                               $protocolVersion
-     *
-     * @return ResponseInterface
-     */
-    public function createResponse(
-        $statusCode = 200,
-        $reasonPhrase = null,
         array $headers = [],
         $body = null,
         $protocolVersion = '1.1'
