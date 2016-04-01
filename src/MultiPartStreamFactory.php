d1cc56442eaf86f0dfaac56552738c0fae732996
Robert Schönthal <caziel@gmx.net>
2016-04-01 14:01:42 +0200

    added new interface for creating multipart streams

---
+++ src/MultiPartStreamFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Http\Message;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Factory for creating Multipart Streams.
+ *
+ * @author Robert Schönthal <robert.schoenthal@gmail.com>
+ */
+interface MultiPartStreamFactory extends StreamFactory
+{
+    /**
+     * creates a Multipart Stream from parameters and/or files.
+     *
+     * @param array $parameters
+     * @param array $files
+     *
+     * @return StreamInterface
+     *
+     * @throws \InvalidArgumentException If the $parameters or $files are invalid.
+     * @throws \RuntimeException         If creating the stream fails.
+     */
+    public function createMultiPartStream(array $parameters = [], array $files = []);
+}
